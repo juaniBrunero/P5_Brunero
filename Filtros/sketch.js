84b4c72c9a73b6978cb96075cf5b79ee37bbf7bc
JuaniBrunero <juanibrunero@gmail.com>
2017-03-19 20:54:46 -0300

    Filtros casi

--- Filtros/sketch.js
+++ Filtros/sketch.js
@@ -1,19 +1,28 @@
 var foto;
 var panda;
-var nueva;
-var nuevo;
 
-var matB = [[1, 2, 1],[2, 4, 2],[1, 2, 1]];
+var temp;
+
+var edgeX;
+var edgeY;
+var edgeSobel;
+var edgeColor;
+
+var nueMax;
+var nueMin;
+
+var matB = [[1, 2,  1],[2, 4,  2],[1 ,  2,  1]];
 var divB = 16;
 
-var matX = [[1, 0, -1],[2, 0, -2],[1, 0, -1]];
+var matX = [[1, 0, -1],[2, 0, -2],[1 ,  0, -1]];
+var matY = [[1, 2,  1],[0, 0,  0],[-1, -2, -1]];
 
 function preload(){
-  foto = loadImage('panda.jpg');
+  foto  = loadImage('panda.jpg');
 }
 
 function setup() {
-  createCanvas(271*2, 186*2);
+  createCanvas(foto.width*2, foto.height*3);
 
   foto.loadPixels();
 
@@ -26,55 +35,84 @@ function setup() {
       brillo = brillo / 3;
       panda[i][j] = brillo;
     }
-  }
+  } //Copio el panda el array en brillo solo
 
   foto.updatePixels();
 
-  nueva = panda;
-
-  nuevo = [];
-
-  for (var i = 1; i < panda.length-1; i++) {
-    nuevo[i] = [];
-    for (var j = 1; j < panda[i].length-1; j++) {
-      nuevo[i][j] = conv(nueva, i, j, matB, divB);
+  edgeX = [];
+  edgeY = [];
+  for (var i = 0; i < panda.length; i++) {
+    edgeX[i] = [];
+    edgeY[i] = [];
+    for (var j = 0; j < panda[i].length; j++) {
+      edgeX[i][j] = panda[i][j];
+      edgeY[i][j] = panda[i][j];
     }
   }
 
-  swap(nuevo, nueva);
+  copiarYAplicar(edgeX, matX);
 
-  for (var i = 1; i < panda.length-1; i++) {
-    for (var j = 1; j < panda[i].length-1; j++) {
-      //nuevo[i][j] = conv(nueva, i, j, matX);
-    }
-  }
+  copiarYAplicar(edgeY, matY);
 
-  swap(nuevo, nueva);
+  edgeSobel = sobel(edgeSobel, edgeX, edgeY);
+  edgeSobel = sacarMedio(edgeSobel);
+
+  edgeColor = colorDe(edgeColor, edgeX, edgeY);
 
 }
 
 function draw() {
   background(255);
 
+  //ORIGINAL
   image(foto, 0, 0);
 
+  //BLANCO Y NEGRO
   for (var i = 0; i < foto.width; i++) {
     for (var j = 0; j < foto.height; j++) {
       stroke(panda[i][j]);
-      point(271+i, j);
+      point(foto.width+i, j);
     }
   }
 
-  for (var i = 0; i < foto.width; i++) {
-    for (var j = 0; j < foto.height; j++) {
-      stroke(nueva[i][j]);
-      point(i, 186+j);
+  //SOBELX
+  for (var i = 1; i < foto.width-1; i++) {
+    for (var j = 1; j < foto.height-1; j++) {
+      stroke(edgeX[i][j]);
+      point(i, foto.height+j);
+    }
+  }
+
+  //SOBELY
+  for (var i = 1; i < foto.width-1; i++) {
+    for (var j = 1; j < foto.height-1; j++) {
+      stroke(edgeY[i][j]);
+      point(foto.width+i, foto.height+j);
+    }
+  }
+
+  //SOBEL MODULO
+  for (var i = 1; i < foto.width-1; i++) {
+    for (var j = 1; j < foto.height-1; j++) {
+      stroke(edgeSobel[i][j]);
+      point(i, foto.height*2+j);
+    }
+  }
+
+  colorMode(HSB, TWO_PI);
+
+  //SOBEL DIRECCION
+  for (var i = 1; i < foto.width-1; i++) {
+    for (var j = 1; j < foto.height-1; j++) {
+      stroke(edgeColor[i][j], 255, edgeSobel[i][j]);
+      point(foto.width+i, foto.height*2+j);
     }
   }
 
   noLoop();
 }
 
+//CONVOLUCION DE MATRIX DE 3X3
 function conv(m, x, y, mat, t=1){
   var sum = 0;
   sum += m[x-1][y-1] * mat[0][0];
@@ -89,16 +127,108 @@ function conv(m, x, y, mat, t=1){
   return sum/t;
 }
 
-function swap(pri, seg){
-  var temp = pri;
-  pri = seg;
-  seg = temp;
+//DEVUELVE EL MAXIMO
+function maximo(m){
+  var maxi = m[m.length-1][m[m.length-1].length-1];
+  for (var i = 1; i < m.length-1; i++) {
+    for (var j = 1; j < m[i].length-1; j++) {
+      maxi = (m[i][j] > maxi)? m[i][j] : maxi;
+    }
+  }
+  return maxi;
 }
 
-function nuevoAr(a, b){
-  a = new Array();
-  for (var i = 0; i < b; i++) {
-    a[i] = new Array();
+//DEVUELVE EL MINIMO
+function minimo(m){
+  var mini = m[1][1];
+  for (var i = 1; i < m.length-1; i++) {
+    for (var j = 1; j < m[i].length-1; j++) {
+      mini = (m[i][j] < mini)? m[i][j] : mini;
+    }
+  }
+  return mini;
+}
+
+//UNA CONVOLUCION
+function copiarYAplicar(m, mat){
+  var nueva = [];
+  for (var i = 1; i < panda.length-1; i++) { //Aplica la convolucion
+    nueva[i] = [];
+    for (var j = 1; j < panda[i].length-1; j++) {
+      nueva[i][j] = conv(m, i, j, mat);
+    }
+  }
+
+  var nueMax = maximo(nueva);
+  var nueMin = minimo(nueva);
+
+  for (var i = 1; i < panda.length-1; i++) { //Copia en el array
+    for (var j = 1; j < panda[i].length-1; j++) {
+      m[i][j] = map(nueva[i][j], nueMax, nueMin, 0, 255);
+    }
+  }
+}
+
+//CALCULO DEL MODULO CON LAS COMPONENTES DEL VECTOR
+function sobel(a, x, y){
+  a = [];
+  for (var i = 1; i < x.length-1; i++) { //Calcula la gradiente
+    a[i] = [];
+    for (var j = 1; j < x[i].length-1; j++) {
+      a[i][j] = sqrt(x[i][j] * x[i][j] + y[i][j] * y[i][j]);
+    }
+  }
+
+  var nueMax = maximo(a);
+  var nueMin = minimo(a);
+
+  for (var i = 1; i < panda.length-1; i++) { //Copia en el array
+    for (var j = 1; j < panda[i].length-1; j++) {
+      a[i][j] = map(a[i][j], nueMax, nueMin, 0, 255);
+    }
+  }
+
+  return a;
+}
+
+//NORMALIZAR VALORES PARA LOS EXTREMOS
+function sacarMedio(a){
+  for (var i = 1; i < a.length-1; i++) { //Copia en el array
+    for (var j = 1; j < a[i].length-1; j++) {
+      a[i][j] = abs(a[i][j] - 255/2);
+      a[i][j] = map(a[i][j], 0, 255/2, 0, 255);
+    }
+  }
+
+  for (var i = 1; i < a.length-1; i++) { //Copia en el array
+    for (var j = 1; j < a[i].length-1; j++) {
+      if(a[i][j] > 120){
+        a[i][j] = 255;
+      }else{
+        a[i][j] = 0;
+      }
+    }
+  }
+  return a;
+}
+
+//EDGE COLOR CON SOBEL
+function colorDe(a, x, y){
+  var a = [];
+  for (var i = 1; i < x.length-1; i++) { //Copia en el array
+    a[i] = [];
+    for (var j = 1; j < x[i].length-1; j++) {
+      a[i][j] = atan(x[i][j] / max(y[i][j], 1));
+    }
+  }
+
+  var nueMax = maximo(a);
+  var nueMin = minimo(a);
+
+  for (var i = 1; i < panda.length-1; i++) { //Copia en el array
+    for (var j = 1; j < panda[i].length-1; j++) {
+      a[i][j] = map(a[i][j], nueMax, nueMin, 0, TWO_PI);
+    }
   }
   return a;
 }
