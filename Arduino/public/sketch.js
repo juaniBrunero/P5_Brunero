d14b8967685ed9766e53770c1e1fcd2de4bc7f7c
JuaniBrunero <juanibrunero@gmail.com>
2017-03-25 17:47:47 -0300

    Agrego app para arduino y p5.js (usando node, express y socket.io)

---
+++ Arduino/public/sketch.js
@@ -0,0 +1,34 @@
+var socket;
+
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+
+  socket = io.connect('http://localhost:3000');
+
+  socket.on('mouse',
+
+    function(data) {
+      console.log("Got: " + data.x + " " + data.y);
+
+      fill(0,0,255);
+      noStroke();
+      ellipse(data.x, data.y, 20, 20);
+    }
+  );
+}
+
+function draw() {
+
+}
+
+function sendmouse(xpos, ypos) {
+  console.log("sendmouse: " + xpos + " " + ypos);
+
+  var data = {
+    x: xpos,
+    y: ypos
+  };
+
+  socket.emit('mouse',data);
+}
